88a41f3241e86a91e6b212afad03492ecd411a21
chenchen2655 <127909260+chenchen2655@users.noreply.github.com>
2023-04-17 19:33:58 +0800

    Add files via upload

---
+++ homework/task1/task.cpp
@@ -0,0 +1,219 @@
+﻿// 基于栈数据结构实现字符串计算器
+#include <iostream>
+#include "Vector/vector.h"
+#include <string>
+#include <cctype>
+using namespace std;
+
+// 定义栈数据结构
+template <typename T>
+class Stack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T data, Node* next = nullptr) : data(data), next(next) {}
+    };
+    Node* top;
+    int size;
+public:
+    Stack() : top(nullptr), size(0) {}
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+    int getSize() const {
+        return size;
+    }
+    void push(T data) {
+        top = new Node(data, top);
+        size++;
+    }
+    void pop() {
+        if (isEmpty()) {
+            throw "Stack is empty";
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        size--;
+    }
+    T getTop() const {
+        if (isEmpty()) {
+            throw "Stack is empty";
+        }
+        return top->data;
+    }
+};
+
+// 定义运算符优先级
+int getPriority(char op) {
+    switch (op) {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    case '(':
+        return 0;
+    default:
+        throw "Invalid operator";
+    }
+}
+
+// 定义运算符结合性
+bool isLeftAssociative(char op) {
+    switch (op) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        return true;
+    case '^':
+        return false;
+    default:
+        throw "Invalid operator";
+    }
+}
+
+// 定义运算符操作
+int operate(int a, int b, char op) {
+    switch (op) {
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        if (b == 0) {
+            throw "Division by zero";
+        }
+        return a / b;
+    case '^':
+        return pow(a, b);
+    default:
+        throw "Invalid operator";
+    }
+}
+
+// 定义字符串计算器函数
+int calculate(string s) {
+    // 去除空格
+    s.erase(remove(s.begin(), s.end(), ' '), s.end());
+
+    // 定义两个栈，一个用于存储操作数，一个用于存储运算符
+    Stack<int> operands;
+    Stack<char> operators;
+
+    // 遍历输入字符串
+    for (int i = 0; i < s.length(); i++) {
+
+        // 如果是数字，就把它转换成整数并压入操作数栈
+        if (isdigit(s[i])) {
+            int num = 0;
+            while (i < s.length() && isdigit(s[i])) {
+                num = num * 10 + (s[i] - '0');
+                i++;
+            }
+            i--;
+            operands.push(num);
+
+            // 如果是左括号，就压入运算符栈
+        }
+        else if (s[i] == '(') {
+            operators.push(s[i]);
+
+            // 如果是右括号，就弹出运算符栈直到遇到左括号，并计算结果
+        }
+        else if (s[i] == ')') {
+            while (!operators.isEmpty() && operators.getTop() != '(') {
+                // 弹出两个操作数和一个运算符
+                int b = operands.getTop();
+                operands.pop();
+                int a = operands.getTop();
+                operands.pop();
+                char op = operators.getTop();
+                operators.pop();
+                // 计算结果并压入操作数栈
+                int result = operate(a, b, op);
+                operands.push(result);
+            }
+            // 如果运算符栈为空或者没有遇到左括号，说明输入的表达式无效
+            if (operators.isEmpty() || operators.getTop() != '(') {
+                throw "Invalid expression";
+            }
+            // 弹出左括号
+            operators.pop();
+
+            // 如果是运算符，就比较它和运算符栈顶的优先级和结合性
+        }
+        else if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^') {
+            while (!operators.isEmpty() &&
+                (getPriority(s[i]) < getPriority(operators.getTop()) ||
+                    (getPriority(s[i]) == getPriority(operators.getTop()) && isLeftAssociative(s[i])))) {
+                // 弹出两个操作数和一个运算符
+                int b = operands.getTop();
+                operands.pop();
+                int a = operands.getTop();
+                operands.pop();
+                char op = operators.getTop();
+                operators.pop();
+                // 计算结果并压入操作数栈
+                int result = operate(a, b, op);
+                operands.push(result);
+            }
+            // 压入当前运算符
+            operators.push(s[i]);
+
+            // 如果是其他字符，说明输入的表达式无效
+        }
+        else {
+            throw "Invalid expression";
+        }
+    }
+
+    // 遍历完输入字符串后，如果运算符栈不为空，就继续计算结果
+    while (!operators.isEmpty()) {
+        // 弹出两个操作数和一个运算符
+        int b = operands.getTop();
+        operands.pop();
+        int a = operands.getTop();
+        operands.pop();
+        char op = operators.getTop();
+        operators.pop();
+        // 计算结果并压入操作数栈
+        int result = operate(a, b, op);
+        operands.push(result);
+    }
+
+    // 如果操作数栈只有一个元素，那么它就是最终的结果，否则说明输入的表达式无效
+    if (operands.getSize() == 1) {
+        return operands.getTop();
+    }
+    else {
+        throw "Invalid expression";
+    }
+}
+
+// 主函数，用于测试字符串计算器函数
+int main() {
+    string s;
+    cout << "请输入一个表达式：" << endl;
+    getline(cin, s);
+    try {
+        int result = calculate(s);
+        cout << "计算结果是：" << result << endl;
+    }
+    catch (const char* msg) {
+        cout << msg << endl;
+    }
+    return 0;
+}
---
+++ homework/task2/task2.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include "../task1/Vector/vector.h"
+#include <deque>
+#include <random>
+
+// 定义一个结构体，用来表示柱子的索引和高度
+struct Bar {
+    int index; // 柱子的索引
+    int height; // 柱子的高度
+};
+
+// 定义一个队列类，用来存储柱子
+class Queue {
+private:
+    std::deque<Bar> data; 
+public:
+    // 判断队列是否为空
+    bool empty() {
+        return data.empty();
+    }
+
+    // 返回队列的大小
+    int size() {
+        return data.size();
+    }
+
+    // 在队尾插入一个柱子
+    void push(Bar bar) {
+        data.push_back(bar);
+    }
+
+    // 在队首删除一个柱子，并返回它
+    Bar pop() {
+        Bar front = data.front();
+        data.pop_front();
+        return front;
+    }
+
+    // 返回队首的柱子，但不删除它
+    Bar peek() {
+        return data.front();
+    }
+};
+// 定义一个函数，用来计算矩形的最大面积
+int maxArea(std::vector<int>& heights) {
+    int max_area = 0; 
+    Queue stack; 
+    for (int i = 0; i < heights.size(); i++) { 
+        while (!stack.empty() && heights[i] < stack.peek().height) { 
+            Bar top = stack.pop(); 
+            int width = stack.empty() ? i : i - stack.peek().index - 1; 
+            int area = top.height * width; 
+            max_area = std::max(max_area, area); 
+        }
+        stack.push(Bar{ i, heights[i] }); 
+    }
+    while (!stack.empty()) { 
+        Bar top = stack.pop(); 
+        int width = stack.empty() ? heights.size() : heights.size() - stack.peek().index - 1; 
+        int area = top.height * width; 
+        max_area = std::max(max_area, area); 
+    }
+    return max_area; // 返回最大面积
+}
+// 生成随机数
+int random(int min, int max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<> dis(min, max); 
+    return dis(gen); // 返回一个在[min, max]范围内的随机数
+}
+
+// 打印柱子数组和最大面积
+void print(std::vector<int>& heights) {
+    std::cout << "高度: [";
+    for (int i = 0; i < heights.size(); i++) {
+        std::cout << heights[i];
+        if (i != heights.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]\n";
+    std::cout << "最大面积为: " << maxArea(heights) << "\n\n";
+}
+
+// 测试数据的个数
+const int p = 10;
+
+// 柱子数组长度的最小值和最大值
+const int MIN_L = 1;
+const int MAX_L = 105;
+// 柱子高度的最小值和最大值
+const int MIN_H = 0;
+const int MAX_H = 104;
+
+//生成测试数据并调用打印函数
+int main() {
+    for (int i = 0; i < p; i++) { // 循环生成10组测试数据
+        int length = random(MIN_L, MAX_L); // 随机生成柱子数组长度
+        std::vector<int> heights(length); // 创建一个长度为length的柱子数组
+        for (int j = 0; j < length; j++) { // 遍历柱子数组
+            heights[j] = random(MIN_H, MAX_H); // 随机生成柱子高度
+        }
+        print(heights);
+    }
+    return 0; 
+}
